dd2f73d274bf248c2033c5e9c0c5cb99b7a6e406
Dimitris Katikaridis <dkatikaridis@gmail.com>
2020-03-18 01:52:13 +0200

    sidebar: s'/gallery/settings'/

--- app/src/main/java/com/scripty/base/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/scripty/base/ui/settings/SettingsFragment.java
@@ -1,22 +1,18 @@
-package com.scripty.base.ui.gallery;
+package com.scripty.base.ui.settings;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.scripty.base.R;
 import com.scripty.base.ui.BaseFragment;
 
-public class GalleryFragment extends BaseFragment {
+public class SettingsFragment extends BaseFragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
