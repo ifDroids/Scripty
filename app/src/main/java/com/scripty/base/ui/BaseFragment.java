316b290ed322db41211ef3141a700aec1e67c7dd
Dimitris Katikaridis <dkatikaridis@gmail.com>
2020-03-18 01:34:26 +0200

    CitMub go home you are drunk

--- app/src/main/java/com/scripty/base/ui/BaseFragment.java
+++ app/src/main/java/com/scripty/base/ui/BaseFragment.java
@@ -1,11 +1,7 @@
-package com.example.scripty.ui;
+package com.scripty.base.ui;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
