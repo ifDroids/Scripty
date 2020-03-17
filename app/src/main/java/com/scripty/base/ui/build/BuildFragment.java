ce4afdb80b4dd7991fbee99b447726e4f1d4d697
Dimitris Katikaridis <dkatikaridis@gmail.com>
2020-03-18 01:56:29 +0200

    sidebar: Replace 'home' menu with 'build'

--- app/src/main/java/com/scripty/base/ui/build/BuildFragment.java
+++ app/src/main/java/com/scripty/base/ui/build/BuildFragment.java
@@ -1,20 +1,16 @@
-package com.scripty.base.ui.home;
+package com.scripty.base.ui.build;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.scripty.base.R;
 import com.scripty.base.ui.BaseFragment;
 
-public class HomeFragment extends BaseFragment {
+public class BuildFragment extends BaseFragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
                return inflater.inflate(R.layout.fragment_home, container, false);
