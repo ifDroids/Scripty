b59a9bbf3784aec8410b204fe1f530b375062d91
Dimitris Katikaridis <dkatikaridis@gmail.com>
2020-03-18 20:12:11 +0200

    Project: Initial dummy layout for commands

--- app/src/main/java/com/scripty/base/ui/build/BuildFragment.java
+++ app/src/main/java/com/scripty/base/ui/build/BuildFragment.java
@@ -1,18 +1,73 @@
 package com.scripty.base.ui.build;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.scripty.base.R;
 import com.scripty.base.ui.BaseFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class BuildFragment extends BaseFragment {
 
+    @BindView(R.id.command_add)
+    FloatingActionButton mCommandAdd;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-               return inflater.inflate(R.layout.fragment_home, container, false);
+        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final LinearLayout commandsContainer = root.findViewById(R.id.commands_container);
+
+        ButterKnife.bind(this,root);
+
+        mCommandAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(_BaseActivity);
+
+                @SuppressLint("InflateParams")
+                final View customLayout = _BaseActivity.getLayoutInflater().inflate(R.layout.command_add_dialog, null);
+                builder.setView(customLayout);
+
+                Button btnOk = customLayout.findViewById(R.id.ok);
+                Button btnCancel = customLayout.findViewById(R.id.cancel);
+
+                btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {}});
+
+                btnOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // append the command
+                        View command = View.inflate(_BaseActivity, R.layout.command_preference, null);
+                        commandsContainer.addView(command);
+                    }
+                });
+
+                final AlertDialog dialog = builder.create();
+                if (dialog.getWindow() != null) {
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                }
+                dialog.show();
+            }
+        });
+
+        return root;
     }
+
 }
