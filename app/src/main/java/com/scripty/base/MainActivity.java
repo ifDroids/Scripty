9b7bd452b2c8463388b24cadbb8c3ae0192d4e52
Dimitris Katikaridis <dkatikaridis@gmail.com>
2020-03-20 04:15:33 +0200

    [ABANDONED] end of wip. back to static tries

--- app/src/main/java/com/scripty/base/MainActivity.java
+++ app/src/main/java/com/scripty/base/MainActivity.java
@@ -1,24 +1,17 @@
 package com.scripty.base;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,61 +25,42 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.nav_view)
     NavigationView mNavView;
 
+    Bundle state;
     private AppBarConfiguration mAppBarConfiguration;
 
-    private LiveData currentNavController;
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-
-        setupSidebar();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.e("dsdsds","222222222");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
-        Log.e("dsdsds","11111111");
-        if(savedInstanceState==null)
-            setupSidebar();
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_settings, R.id.nav_help)
+                .setDrawerLayout(mDrawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(mNavView, navController);
+
+
 
     }
 
-    private void setupSidebar(){
-        CustomNavigationView navView = findViewById(R.id.nav_view);
-        List<Integer> navGraphIds = new ArrayList<>();
-        navGraphIds.add(R.navigation.home);
-        navGraphIds.add(R.navigation.settings);
-        navGraphIds.add(R.navigation.help);
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
 
-        LiveData lifecycler = navView.setupWithNavController(
-                navGraphIds,getSupportFragmentManager(),R.id.nav_host_fragment,getIntent());
-
-
-        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        final AppCompatActivity m = this;
-        lifecycler.observe(this, new Observer() {
-            @Override
-            public void onChanged(Object o) {
-                NavigationUI.setupActionBarWithNavController(m,navController);
-            }
-        });
-        currentNavController = lifecycler;
     }
+
+
+
     @Override
     public boolean onSupportNavigateUp() {
-        LiveData x = currentNavController;
-        if (x != null){
-            NavController z = (NavController) x.getValue();
-            if(z!=null){
-                return z.navigateUp();
-            }
-        }
-        return false;
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/com/scripty/base/ui/fragments/build/BuildFragment.java
+++ app/src/main/java/com/scripty/base/ui/fragments/build/BuildFragment.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +13,15 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.scripty.base.R;
 import com.scripty.base.ui.BaseFragment;
 import com.scripty.base.ui.views.CommandLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,11 +31,38 @@ public class BuildFragment extends BaseFragment {
     @BindView(R.id.command_add)
     FloatingActionButton mCommandAdd;
 
+    private static boolean needsUIRerender=false;
+    private static List<CommandLayout> views=new ArrayList<>();
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.fragment_home, container, false);
+
         final LinearLayout commandsContainer = root.findViewById(R.id.commands_container);
 
+        // its always 0 .
+        Log.e("DDDD",commandsContainer.getChildCount()+"");
         ButterKnife.bind(this,root);
+            if (needsUIRerender) {
+
+                    Log.e("dsdsdsd","---------");
+                    for (int i = 0 ; i < views.size();i++) {
+                        Log.e("sdsdsd","==============");
+                        if(commandsContainer.getParent() != null) {
+                            ((ViewGroup) commandsContainer.getParent()).removeView(commandsContainer);
+                            commandsContainer.removeAllViews();
+                            commandsContainer.removeAllViewsInLayout();
+                            commandsContainer.addView(views.get(i));
+                        }
+                    }
+
+                needsUIRerender=false;
+            }
 
         mCommandAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +86,14 @@ public class BuildFragment extends BaseFragment {
                     public void onClick(View v) {
                         // append the command
                         CommandLayout command = new CommandLayout(_BaseActivity);
+                        commandsContainer.removeView(command);
                         commandsContainer.addView(command);
+
+
+                        needsUIRerender=true;
+                        views.add(command);
+                        Log.e("DDDD",commandsContainer.getChildCount()+"");
+
                     }
                 });
 
@@ -73,7 +112,14 @@ public class BuildFragment extends BaseFragment {
             }
         });
 
+
+       // if(needsUISave) s.saveState(this,"build");
         return root;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 
 }
